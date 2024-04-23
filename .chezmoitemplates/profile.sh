cc10dd75f7f91ba6e1fd9de0f6dfa02e4e5d56d3
nnmchang <121856985+nnmchang@users.noreply.github.com.>
2024-04-23 15:04:06 +0900

    fix: エイリアス設定をrc系ファイルに移動

--- .chezmoitemplates/profile.sh
+++ .chezmoitemplates/profile.sh
@@ -18,11 +18,6 @@ if [ -e "$HOME/.cargo/env" ]; then
     . "$HOME/.cargo/env"
 fi
 
-alias ls='lsd'
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
 # Expand $PATH to include the directory where snappy applications go.
 snap_bin_path="/snap/bin"
 if [ -n "${PATH##*${snap_bin_path}}" -a -n "${PATH##*${snap_bin_path}:*}" ]; then

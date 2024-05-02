b4595af446173b482e4bdf8f9c3dceb8b7d91672
NNMchang <nnmchang@NNM-MBP.local>
2024-05-03 03:25:18 +0900

    remove: neovim config remove

--- private_dot_config/nvim/lua/plugins/nvim-colorizer.lua
+++
@@ -1,8 +0,0 @@
--- FIXME: windowsで実行した際にカラー表示されない
-return { {
-  'norcalli/nvim-colorizer.lua',
-  opts = { user_default_options = { names = false } },
-  config = function(_, opts)
-    require 'colorizer'.setup(opts)
-  end
-} }

78a300aa86a3b909144fa95af5a56edf7613fbb8
nnmchang <121856985+nnmchang@users.noreply.github.com>
2024-05-27 18:28:05 +0900

    add: bat, yazi

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -15,3 +15,14 @@ Set-Alias -Name: "ls" -Value: "lsd"
 function ll {
     ls -l $args
 }
+
+$env:YAZI_FILE_ONE=$(join-path $(where.exe git) ../../usr/bin/file.exe)
+function yy {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
